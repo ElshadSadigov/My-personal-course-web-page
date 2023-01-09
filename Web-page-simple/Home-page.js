7989c2c74e16bf41739fff6744f6d4e801c36c14
ElshadSadigov <Sv.Elshad@gmail.com>
2023-01-09 14:39:45 +0400

    Website developed with HTML, CSS and JS
    Full-stack tutorials are displayed for you on the website

---
+++ Web-page-simple/Home-page.js
@@ -0,0 +1,8 @@
+let header = document.querySelector('.header');
+// headerUl = document.querySelector('.header-ul');
+resBtn = document.querySelector('.res-btns');
+
+resBtn.addEventListener('click',()=>{
+    header.classList.toggle('active')
+    // console.log('ssl')
+})
